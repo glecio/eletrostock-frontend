73bde9d873058b0b1a20ccb7a097da9ce1d4df32
Glecio Santos <glecio.santos@gmail.com>
2022-06-16 16:47:51 -0300

    Brands list page

--- src/components/brands/BrandList/BrandList.jsx
+++ src/components/brands/BrandList/BrandList.jsx
@@ -2,6 +2,8 @@ import React  from 'react';
 import { DeviceService } from "../../../services/DeviceServices";
 import {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 
 
 let BrandList = () => {
@@ -22,10 +24,22 @@ let BrandList = () => {
     fetchData();
 }, []);
 
+
+const handleDelete = async(e) => {
+    //e.preventDefault();
+        const element = document.querySelector('#message2');
+        const headers = { 
+            "Content-Type": "multipart/form-data"
+        };
+        const response = await axios.delete('http://localhost:8080/api/brands/${e}')
+        .then(() => element.innerHTML = 'Delete successful');
+      
+}
+
 return (
         <>
             <div className="container">
-                <h2>Brands List</h2>
+                <h2 className='my-3'>Brands List</h2>
                 <Link to={`/brands/add`} className='btn btn-sm btn-outline-primary mx-1'> Add Brand</Link>
 
                 <table className="table table-striped table-hover">
@@ -42,7 +56,7 @@ return (
                                 <td className='col-6 text-center'>
                                     <Link to={`/brands/view/${brand.id}`} className='btn btn-sm btn-outline-primary mx-1'> View</Link>
                                     <Link to={`/brands/edit/${brand.id}`} className='btn btn-sm btn-outline-success mx-1'> Edit</Link>
-                                    <Link to={`/brands/view/${brand.id}`} className='btn btn-sm btn-outline-danger mx-1'> Delete</Link>
+                                    <button onClick={()=> handleDelete(brand.id)} className='btn btn-sm btn-outline-danger mx-1'> Delete</button>
                                 </td>
                             </tr>
                         )}
